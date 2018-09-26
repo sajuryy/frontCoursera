b3592b7c1f703bc3f04805c7f02b632459b15204
sajuryy <sajuryy@gmail.com>
2018-09-26 17:54:04 +0300

    week 3 add and subtract

--- week3/timeControll/index.js
+++ week3/timeControll/index.js
@@ -6,28 +6,73 @@
 module.exports = function (date) {
     const time = {
         value: new Date(date),
-        add: (amount, measure) => {
-            // console.log(amount, measure)
-            return time;
+        add: function (amount, measure) {
+            const ddd = new Date(this.value);
+
+            if (measure === 'years') {
+                ddd.setFullYear(ddd.getFullYear() + amount);
+            }
+
+            if (measure === 'months') {
+                ddd.setMonth(ddd.getMonth() + amount);
+            }
+
+            if (measure === 'days') {
+                ddd.setDate(ddd.getDate() + amount);
+            }
+
+            if (measure === 'hours') {
+                ddd.setHours(ddd.getHours() + amount);
+            }
+
+            if (measure === 'minutes') {
+                ddd.setMinutes(ddd.getMinutes() + amount);
+            }
+
+            this.value = formatDate(ddd);
+            return this;
         },
-        subtract: (amount, measure) => {
-            // console.log(amount, measure)
-            return time;
+        subtract: function (amount, measure) {
+            const ddd = new Date(this.value);
+            if (measure === 'years') {
+                ddd.setFullYear(ddd.getFullYear() - amount);
+            }
+
+            if (measure === 'months') {
+                ddd.setMonth(ddd.getMonth() - amount);
+            }
+
+            if (measure === 'days') {
+                ddd.setDate(ddd.getDate() - amount);
+            }
+
+            if (measure === 'hours') {
+                ddd.setHours(ddd.getHours() - amount);
+
+            }
+            if (measure === 'minutes') {
+                ddd.setMinutes(ddd.getMinutes() - amount);
+            }
+
+            this.value = formatDate(ddd);
+            return this;
         }
     };
 
-    console.log(formatDate(time.value));
+    time.value = formatDate(time.value);
     return time;
+
 };
+
 
 
 
 const formatDate = (date) => {
     const year = date.getFullYear();
     const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
-    const day = (date.getDay() < 10) ? `0${date.getDay()}` : date.getDay();
-    const hours = date.getHours();
-    const minuts = date.getMinutes();
+    const day = (date.getDate() < 10) ? `0${date.getDate()}` : date.getDate();
+    const hours = (date.getHours() < 10) ? `0${date.getHours()}` : date.getHours();
+    const minuts = (date.getMinutes() < 10) ? `0${date.getMinutes()}` : date.getMinutes()
 
     return `${year}-${month}-${day} ${hours}:${minuts}`;
 
